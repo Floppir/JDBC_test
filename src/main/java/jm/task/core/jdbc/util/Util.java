053c2ac9d90f36bdd672831ee428e4967b8cfb2d
Floppir <dvgstadium@gmail.com>
2023-09-14 19:33:07 +0300

    3 commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -10,12 +10,14 @@ public class Util {
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
 
-    public static Connection getConnection() throws SQLException
-    {
-        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        return connection;
     }
 
     public static void closeConnection() throws SQLException {
-        getConnection().close();
+        connection.close();
     }
 }
